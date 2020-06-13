665e1ff7790b4a5da4d34aa7f80e99d8660e1228
Dimitrije Karanfilović <dimitrije.karanfilovic@uns.ac.rs>
2020-06-13 23:39:08 +0200

    Started basic entities implementation.

--- src/entities/Activity.java
+++ src/entities/Activity.java
@@ -1,10 +1,68 @@
 package entities;
 
-public class Activity implements Comparable<Activity>{
+import java.time.LocalDateTime;
 
+public class Activity implements Comparable<Activity>{
+    private String description;
+    private LocalDateTime time;
+    private int amount;
+    private Currency currency;
+    private ActivityVersion activityVersion;
+
+    public Activity(String description, LocalDateTime time, int amount, Currency currency, ActivityVersion activityVersion) {
+        this.description = description;
+        this.time = time;
+        this.amount = amount;
+        this.currency = currency;
+        this.activityVersion = activityVersion;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public void setActivityVersion(ActivityVersion activityVersion) {
+        this.activityVersion = activityVersion;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public ActivityVersion getActivityVersion() {
+        return activityVersion;
+    }
 
     @Override
     public int compareTo(Activity o) {
         return 0;
+    }
+
+    public boolean isBetween(LocalDateTime fromDate, LocalDateTime toDate){
+        return this.time.isAfter(fromDate) && this.time.isBefore(toDate);
     }
 }

be68fc0a4cd8d9c0c2e7dfdaf982df4d0c9fa873
Dimitrije Karanfilović <dimitrije.karanfilovic@uns.ac.rs>
2020-06-14 00:13:20 +0200

    Added necessary .jar files.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
     }
 }

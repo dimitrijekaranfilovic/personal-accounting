fa42541deb8f20456fb8e2a9c60bde92c90c242b
Dimitrije Karanfilović <dimitrije.karanfilovic@uns.ac.rs>
2020-06-13 23:39:08 +0200

    Started basic entities implementation.

--- src/entities/Balance.java
+++ src/entities/Balance.java
@@ -1,4 +1,38 @@
 package entities;
 
-public class Balance {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Balance implements  Comparable<Balance>{
+    private LocalDateTime time;
+    private ArrayList<Integer> currencyBalances;
+
+    public Balance(LocalDateTime time) {
+        this.time = time;
+        this.currencyBalances = new ArrayList<Integer>();
+        for(int i = 0; i < Currency.values().length;i++){
+            this.currencyBalances.add(0);
+        }
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public ArrayList<Integer> getCurrencyBalances() {
+        return currencyBalances;
+    }
+
+    public void setCurrencyBalances(ArrayList<Integer> currencyBalances) {
+        this.currencyBalances = currencyBalances;
+    }
+
+    @Override
+    public int compareTo(Balance o) {
+        return 0;
+    }
 }

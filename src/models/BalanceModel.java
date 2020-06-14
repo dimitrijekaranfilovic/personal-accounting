bc5be7eca39975608906ad35d5a155f863c7600e
Dimitrije Karanfilović <dimitrije.karanfilovic@uns.ac.rs>
2020-06-14 17:46:10 +0200

    Improved table models.

--- src/models/BalanceModel.java
+++ src/models/BalanceModel.java
@@ -1,17 +1,23 @@
 package models;
 
 import entities.Balance;
+import entities.Currency;
 
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 
 public class BalanceModel extends AbstractTableModel {
     public ArrayList<Balance> balances;
-    private String[] columnNames;
+    private ArrayList<String> columnNames;
 
     //TODO: mora se voditi racuna o tome da se mogu dodavati valute
     public BalanceModel(ArrayList<Balance> balances) {
         this.balances = balances;
+        this.columnNames = new ArrayList<>();
+        for(Currency c: Currency.values()){
+            columnNames.add(c.toString());
+        }
+        columnNames.add("Date");
     }
 
 
@@ -30,11 +36,26 @@ public class BalanceModel extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return this.columnNames.length;
+        return this.columnNames.size();
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        Balance b = this.balances.get(rowIndex);
+        if(columnIndex < Currency.values().length)
+            return b.getCurrencyBalances().get(columnIndex);
+        else
+            return b.getTime();
+        //return null;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return this.getValueAt(0, columnIndex).getClass();
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames.get(column);
     }
 }
